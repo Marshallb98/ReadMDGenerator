e003d5737e1d60d02555b03a5203c136ca32eee3
Marshall Bertschy <basemarsh@gmail.com>
2020-12-17 21:14:02 -0600

    redid the inquirer questions

--- index.js
+++ index.js
@@ -1,56 +1,7 @@
 const genMarkdown = require('./utils/generateMarkdown')
 const inquirer = require('inquirer');
 const fs = require('fs');
-// array of questions for user
-const questions = [
 
-    {
-        type: 'input',
-        name: 'title',
-        message: 'What is the name of your Project?',
-    },
-    {
-        type: 'input',
-        name: 'description',
-        message: 'Give a brief description of the Project.',
-    },
-    {
-        type: 'input',
-        name: 'install',
-        message: 'Give some instructions on how to install your Project!',
-    },
-    {
-        type: 'input',
-        name: 'usage',
-        message: 'What are some usages for this Project?',
-    },
-    {
-        type: 'input',
-        name: 'guidelines',
-        message: 'What are some contribution guidelines?',
-    },
-    {
-        type: 'input',
-        name: 'test',
-        message: 'Give some instructions on how to test your Project!',
-    },
-    {
-        type: 'list',
-        name: 'license',
-        message: 'What license are you using?',
-        choices: ['apache-2.0', 'artistic-2.0', 'mpl-2.0','mit', 'osl-3.0', 'ms-pl','gpl-3.0', 'lppl-1.3c', 'unlicense'],
-    },
-    {
-        type: 'input',
-        name: 'gituser',
-        message: 'What is your GitHub username?',
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'What is your email?',
-    },
-];
 
 // function to write README file
 function writeToFile(fileName, data) {
@@ -58,7 +9,59 @@ function writeToFile(fileName, data) {
 
 // function to initialize program
 function init() {
-
+        inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'title',
+                message: 'What is the name of your Project?',
+            },
+            {
+                type: 'input',
+                name: 'description',
+                message: 'Give a brief description of the Project.',
+            },
+            {
+                type: 'input',
+                name: 'install',
+                message: 'Give some instructions on how to install your Project!',
+            },
+            {
+                type: 'input',
+                name: 'usage',
+                message: 'What are some usages for this Project?',
+            },
+            {
+                type: 'input',
+                name: 'guidelines',
+                message: 'What are some contribution guidelines?',
+            },
+            {
+                type: 'input',
+                name: 'test',
+                message: 'Give some instructions on how to test your Project!',
+            },
+            {
+                type: 'list',
+                name: 'license',
+                message: 'What license are you using?',
+                choices: ['apache-2.0', 'artistic-2.0', 'mpl-2.0','mit', 'osl-3.0', 'ms-pl','gpl-3.0', 'lppl-1.3c', 'unlicense'],
+            },
+            {
+                type: 'input',
+                name: 'gituser',
+                message: 'What is your GitHub username?',
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'What is your email?',
+            },
+        ])
+        .then((answer) => {
+            
+        }
+        )
 }
 
 // function call to initialize program
